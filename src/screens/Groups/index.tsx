e5e4a216b4c6d399daadbe2219b4ee45dc10a967
Matheus Ramos <matheuseduardosramos@gmail.com>
2022-10-14 20:43:02 -0300

    [FEATURE] Add styled components and define theme

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import * as S from './styles';
+
+export function Groups() {
+    return (
+        <S.Container>
+            <S.Title>Oie</S.Title>
+        </S.Container>
+    );
+}
